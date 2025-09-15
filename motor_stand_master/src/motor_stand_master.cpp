4e6dc3f3149b9f0fc109d895f6f7d89381b8c588
EShi538 <origami1685@gmail.com>
2025-09-15 17:46:33 +0200

    added files

---
+++ motor_stand_master/src/motor_stand_master.cpp
@@ -0,0 +1,372 @@
+#include <Arduino.h>
+#include <motor_stand_master_definitions.h>
+
+////////////////////////////////////////////////////////////////////////////////////////
+//HELPER FUNCTIONS:
+
+void lcd_home(){
+  input = "";
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print(parameter_names[parameter_index] + parameter_values[parameter_index]);
+  lcd.setCursor(0, 2);
+  lcd.print("NEXT: " +  String(ENTER_INPUT) + " | BACK: " + String(BACK_BUTTON));
+  lcd.setCursor(0, 3);
+  lcd.print("THROTTLE:OFF");
+  lcd.setCursor(0, 1);
+}
+
+void tare_ui(){
+  input = "";
+  lcd.clear();
+  lcd.print(tare_names[tare_index] + tare_values[tare_index]);
+  lcd.setCursor(0, 2);
+  lcd.print("NEXT: " +  String(ENTER_INPUT) + "             ");
+  lcd.setCursor(0, 3);
+  if(tare_index == 0){
+    lcd.print("UNITS: N.mm");
+  }
+  else{
+    lcd.print("UNITS: N");
+  }
+  lcd.setCursor(0, 1);
+}
+
+void send_ui(){
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("PRESS " + String(SEND_INPUT) + " TO TARE");
+  if(tare_index == 2){
+    lcd.setCursor(0, 1);
+    lcd.print("ANALOG SENSORS");
+  }
+  if(tare_index != 2){
+    lcd.setCursor(0, 3);
+    lcd.print("BACK: " + String(BACK_BUTTON));
+  }
+  lcd.setCursor(0, 1);
+}
+
+void send_parameters(String type, String value){
+  String signal = type + value;
+  int length = signal.length();
+  Wire.beginTransmission(9);
+  Wire.write(signal.c_str(), length);
+  Wire.endTransmission();
+}
+
+void start_testing(){
+  lcd.clear();
+  lcd.print("RUNNING TEST");
+  lcd.setCursor(0, 1);
+  lcd.print("TEST #: " + parameter_values[0]);
+  lcd.setCursor(0, 2);
+  lcd.print("INCR. LENGTH: " + parameter_values[4] + " s");
+  lcd.setCursor(0, 3);
+  lcd.print("THROTTLE:0");
+  Serial.println("Starting: Test Num: " + parameter_values[0] + " | Increment: " + String(throttleIncrement));
+  start_motor = true;
+  Wire.beginTransmission(9);
+  Wire.write('b');
+  Wire.endTransmission();
+  prev_interval_timestamp = millis();
+}
+
+void end_testing(){
+  Wire.beginTransmission(9);
+  Wire.write('e');
+  Wire.endTransmission();
+  setup();
+}
+
+void throttle_up(){
+  if(cycle_length >= MAX_THROTTLE){
+    if(millis() >= prev_interval_timestamp + INCREMENT_TIME){
+      Serial.println("DONE THROTTLING");
+      done_throttling = true;
+    }
+  }
+  else{
+    if(millis() >= prev_interval_timestamp + INCREMENT_TIME){
+      if(!read_gradient){
+        Wire.beginTransmission(9);
+        Wire.write('w');
+        Wire.endTransmission();
+      }
+      int next_cycle_length = min(cycle_length + pwm_increment, MAX_THROTTLE);
+      for(; cycle_length <= next_cycle_length; cycle_length++){
+        if(done_throttling){
+          return; //return to the main loop and throttle down
+        }
+        esc.writeMicroseconds(cycle_length);
+        lcd.setCursor(9, 3);
+        lcd.print(String(map(cycle_length, 1000, 2000, 0, 100)));
+        delay(THROTTLE_UP_DELAY);
+      }
+      cycle_length--;
+      if(!read_gradient){
+        Wire.beginTransmission(9);
+        Wire.write('g');
+        Wire.endTransmission();
+      }
+      prev_interval_timestamp = millis();
+    }
+  }
+}
+
+void throttle_down(){
+  if(!read_gradient){
+    Wire.beginTransmission(9);
+    Wire.write('w');
+    Wire.endTransmission();
+  }
+  for(int i = cycle_length; i >= MIN_THROTTLE; i--){
+    esc.writeMicroseconds(i);
+    int throttle = map(i, 1000, 2000, 0, 100);
+    if(throttle == 99){
+      lcd.setCursor(11, 3);
+      lcd.print(" ");
+    }
+    if(throttle == 9){
+      lcd.setCursor(10, 3);
+      lcd.print(" ");
+    }
+    lcd.setCursor(9, 3);
+    lcd.print(String(throttle));
+    delay(THROTTLE_UP_DELAY);
+  }
+  delay(INCREMENT_TIME);
+  end_testing();
+}
+
+void interrupt(){
+  if(start_motor){
+    done_throttling = true;
+  }
+}
+
+void setup_next_input(){
+  if(parameter_index < PARAMETER_NUM){
+    parameter_values[parameter_index] = input;
+  }
+  parameter_index++;
+  if(parameter_index == PARAMETER_NUM){
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("SMOOTH DATA?");
+    lcd.setCursor(0, 3);
+    lcd.print("YES: A | NO: B");
+  }
+  else if(parameter_index == PARAMETER_NUM + 1){
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("PRESS " + String(SEND_INPUT) + " TO START");
+  }
+  else{
+    lcd_home();
+  }
+}
+
+void setup_prev_input(){
+  parameter_index = min(parameter_index - 1, PARAMETER_NUM - 1);
+  lcd_home();
+}
+
+void send_inputs(){
+  send_parameters("f", parameter_values[0]);
+  delay(100);
+
+  int max_throttle_input = min(max(parameter_values[1].toInt(), 0), 100);
+  MAX_THROTTLE = map(max_throttle_input, 0, 100, 1000, 2000);
+
+  throttleIncrement = min(parameter_values[2].toInt(), max_throttle_input);
+  pwm_increment = map(throttleIncrement, 0, 100, 0, 1000); //1% -> 99% written in terms of PWM cycle length, assuming a linear mapping
+  
+  send_parameters("m", parameter_values[3]);
+
+  INCREMENT_TIME = parameter_values[4].toInt() * 1000;
+  Serial.println("TEST PARAMETERS CONFIRMED");
+
+  start_testing();
+}
+
+////////////////////////////////////////////////////////////////////////////////////////
+//MAIN DRIVER CODE:
+
+void setup() {
+  pinMode(INTERRUPT_PIN, INPUT_PULLUP); //set default switch position to HIGH
+  attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), interrupt, FALLING); //when switch is pressed down
+
+  for(int i = 0; i < PARAMETER_NUM; i++){
+    parameter_values[i] = "";
+  }
+  input = "";
+  parameter_index = 0;
+
+  done_throttling = false;
+  throttling_up = false;
+  start_motor = false;
+  cycle_length = MIN_THROTTLE;
+  
+  // Set up the LCD display
+  lcd.init();
+  lcd.backlight();
+  //Initialize Serial 
+  Serial.begin(9600);
+  Serial.println("Setting up");
+  lcd.print("LOADING...");
+
+  //Initialize I2C protocol (master)
+  Wire.begin();
+
+  //Initialize servo PWM and arm the ESC
+  esc.attach(ESC_PIN); //set esc to pin
+  esc.writeMicroseconds(MIN_THROTTLE); //minimum throttle; arm the esc
+
+  while(1){
+    Wire.requestFrom(9, 1);
+    if(Wire.read() == 1){
+      break;
+    }
+    delay(100);
+  }
+
+  lcd.setCursor(0, 0);
+  lcd.print("USE PREVIOUS TARE?");
+  lcd.setCursor(0, 3);
+  lcd.print("YES: A | NO: B");
+  choosing = true;
+  tared = false;
+
+  Serial.println("READY");
+}
+
+void loop() {
+  char key = keypad.getKey();
+  
+  if(!tared){
+    if(key){
+      if(!choosing){
+        if(!sending){
+          if(key >= '0' && key <= '9' && input.length() < 9){
+            input += key;
+            lcd.print(key);
+          }   
+          else if(key == ENTER_INPUT && input != "" && tare_index < TARE_NUM){
+            tare_values[tare_index] = input;
+            input = "";
+            send_ui();
+            sending = true;
+          }
+        }
+        else{
+          if(key == BACK_BUTTON && tare_index != 2){
+            tare_ui();
+            sending = false;
+          }
+          else if(key == SEND_INPUT){ //the button to zero the values
+            lcd.setCursor(0, 1);
+            lcd.print("CALIBRATING...");
+            if(tare_index == 0){
+              send_parameters("q", tare_values[tare_index]); //tell slave to tare torque
+              while(1){
+                Wire.requestFrom(9, 1);
+                if(Wire.read() == 1){
+                  break;
+                }
+                delay(100);
+              }
+              tare_index++;
+              tare_ui();
+              sending = false;
+            }
+            else if(tare_index == 1){
+              send_parameters("r", tare_values[tare_index]); //tell slave to tare thrust
+              while(1){
+                Wire.requestFrom(9, 1);
+                if(Wire.read() == 1){
+                  break;
+                }
+                delay(100);
+              }
+              tare_index++;
+              send_ui();
+            }
+            else if(tare_index == 2){ //tell slave to tare analog sensors
+              Wire.beginTransmission(9);
+              Wire.write('a');
+              Wire.endTransmission();
+              while(1){
+                Wire.requestFrom(9, 1);
+                if(Wire.read() == 1){
+                  break;
+                }
+                delay(100);
+              }
+              lcd_home();
+              tared = true;
+              sending = false;
+            }
+          }
+        }
+      }
+      else{
+        if(key == 'A'){
+          Wire.beginTransmission(9);
+          Wire.write('p');
+          Wire.endTransmission();
+          delay(100);
+          choosing = false;
+          tared = true;
+          parameter_index = 0;
+          lcd_home();
+        }
+        else if(key == 'B'){
+          tare_index = 0;
+          sending = false;
+          choosing = false;
+          tare_ui();
+        }
+      }
+    }
+  }
+  else{ //can only do everything else once sensors have been tared
+    if(start_motor){
+      throttle_up();
+    }
+
+    if(done_throttling){
+      Serial.println("THROTTLING DOWN");
+      throttle_down();
+    }
+
+    //if a keystroke has been entered from the keypad
+    if(key){
+      if(key == BACK_BUTTON && parameter_index > 0){
+        setup_prev_input();
+      }
+      else if(key == ENTER_INPUT && input != "" && parameter_index < PARAMETER_NUM){
+        setup_next_input();
+      }
+      else if(parameter_index == PARAMETER_NUM){
+        if(key == 'A'){
+          read_gradient = true;
+          setup_next_input();
+        }
+        else if(key == 'B'){
+          read_gradient = false;
+          setup_next_input();
+        }
+      }
+      else if(key == SEND_INPUT && parameter_index == PARAMETER_NUM + 1){
+        send_inputs();
+      }
+      else if(key >= '0' && key <= '9'){
+        if(input.length() < 3){ 
+          input += key;
+          lcd.print(key);
+        }
+      }
+    }
+  }
+}
---
+++ motor_stand_slave/src/motor_stand_slave.cpp
@@ -0,0 +1,350 @@
+#include <Arduino.h>
+#include <motor_stand_slave_definitions.h>
+
+////////////////////////////////////////////////////////////////////////////////////////
+//HELPER FUNCTIONS
+
+//called when a signal is sent from master
+void receiveEvent(int bytes){ 
+  signal = "";
+  char type = Wire.read(); //get first character 
+  while (Wire.available()) {  // Loop through all available data
+    char c = Wire.read();  // Read a byte
+    signal += c;  // Append to the string
+  }
+
+  if(type == 'f'){ //write to flie
+    new_file_created = true;
+  }
+  else if(type == 'm'){ //set marker
+    marker_sent = true;
+  }
+  else if(type == 'q'){ //torque
+    zero_torque = true;
+  }
+  else if(type == 'r'){ //thrust
+    zero_thrust = true;
+  }
+  else if(type == 'a'){ //analog
+    zero_analog_sensors = true;
+  }
+  else if(type == 'p'){ //previous
+    use_prev_calibration = true;
+  }
+  else if(type == 'b'){ // START data collection
+    reading_on = true;
+  }
+  else if(type == 'e'){ // STOP data collection
+    stop = true;
+    reading_on = false;
+  }
+  else if(type == 'w'){ // PAUSE data collection
+    paused = true;
+  }
+  else if(type == 'g'){
+    paused = false;
+  }
+}
+
+void requestEvent(){
+  Wire.write(ready); //tells the master initialization status
+}
+
+void count(){
+  see_object = true;
+}
+
+void increment(){
+  if(see_object){
+    objects++;
+    see_object = false;
+  }
+}
+
+float zero_analog(float (*func)(), int address){
+  unsigned long start_time = millis();
+  float average_raw = 0;
+  float samples = 0;
+  while(millis() < start_time + 2000){
+    samples++;
+    float reading = func();
+    average_raw += reading;
+
+    Serial.print(F("READING: "));
+    Serial.print(reading);
+    Serial.println(F(" KNOWN: 0"));   
+    delay(5); 
+  }
+  average_raw = average_raw / samples;
+  EEPROM.put(address, average_raw);
+  return average_raw;
+}
+
+void calibrate_hx711(HX711_ADC& load_cell, float known, int address){
+  load_cell.setCalFactor(1);
+  unsigned long start_time = millis();
+  float average_raw = 0;
+  float samples = 0;
+  while(millis() < start_time + 4000){
+    if(load_cell.update()){
+      float reading = load_cell.getData();
+      if(millis() > start_time + 2000){
+        samples++;
+        average_raw += reading;
+
+        Serial.print(F("READING: "));
+        Serial.print(reading);
+        Serial.print(F(" KNOWN: "));
+        Serial.println(known);
+      }
+    }
+  }
+  average_raw = average_raw / samples;
+  float cal_factor = average_raw / known;
+  load_cell.setCalFactor(cal_factor);
+  EEPROM.put(address, cal_factor);
+}
+
+// Initializes Load Cell
+void init_LoadCell () {
+  Serial.println(F("Initializing the HX711 . . ."));
+
+  TorqueSensor.begin();
+  ThrustSensor.begin();
+  
+  boolean _tare = true; //set this to false if you don't want tare to be performed in the next step
+  TorqueSensor.start(2000, _tare); //tare for 2 seconds
+  ThrustSensor.start(2000, _tare);
+
+  if (TorqueSensor.getTareTimeoutFlag() || TorqueSensor.getSignalTimeoutFlag()) {
+    Serial.println(F("Torque Sensor Timeout, check MCU>HX711 wiring and pin designations"));
+    while (1);
+  }
+  
+  if (ThrustSensor.getTareTimeoutFlag() || ThrustSensor.getSignalTimeoutFlag()) {
+    Serial.println(F("Thrust Sensor Timeout, check MCU>HX711 wiring and pin designations"));
+    while (1);
+  }
+
+  Serial.println(F("Done initializing HX711"));
+}
+
+extern int __heap_start, *__brkval;
+int free_memory() {
+  int v;
+  return (int)&v - (__brkval == 0 ? (int)&__heap_start : (int)__brkval);
+}
+
+////////////////////////////////////////////////////////////////////////////////////////
+//MAIN DRIVER CODE
+
+void setup(){
+  signal = "";
+  reading_on = false;
+  stop = false;
+  new_file_created = false;
+  marker_sent = false;
+  zero_torque = false;
+  zero_thrust = false;
+  paused = false;
+  RPM = 0;
+  ready = false;
+  last_serial_timestamp = 0;
+  for(int i = 0; i < 5; i++){
+    measurements[i] = 0;
+  }
+
+  pinMode(CURRENT_PIN, INPUT);
+  pinMode(VOLTAGE_PIN, INPUT);
+
+  pinMode(RPM_PIN, INPUT);
+  see_object = false;
+  attachInterrupt(digitalPinToInterrupt(RPM_PIN), count, CHANGE);
+
+  //Initialize I2C protocol (slave)
+  Wire.begin(9); //Slave arduino set to address 9
+  Wire.onReceive(receiveEvent);
+  Wire.onRequest(requestEvent);
+
+  //Initialize Serial
+  Serial.begin(57600);
+  Serial.println(F("Setting up"));
+  Serial.print(F("Free RAM (in bytes): "));
+  Serial.println(free_memory());
+
+  init_LoadCell(); //initialze the load cell
+
+  //Initialize SD card; If no file is attached or something else goes wrong, 
+  //the code put itself in an infinite loop
+  if (!SD.begin(SD_PIN)) {
+    Serial.println(F("Failed to initialize SD card"));
+    while(1); //infinite loop to prevent further looping by loop()
+  }
+  ready = true;
+}
+
+void loop(){
+  if(use_prev_calibration){
+    Serial.println(F("Retrieving calibration factors"));
+
+    float torque_calibration_factor;
+    EEPROM.get(0, torque_calibration_factor);
+    TorqueSensor.setCalFactor(torque_calibration_factor);
+    Serial.print(F("Torque: "));
+    Serial.println(String(torque_calibration_factor));
+
+    float thrust_calibration_factor;
+    EEPROM.get(10, thrust_calibration_factor);
+    ThrustSensor.setCalFactor(thrust_calibration_factor);
+    Serial.print(F("Thrust: "));
+    Serial.println(String(thrust_calibration_factor));
+
+    EEPROM.get(20, zeroVoltage);
+    Serial.print(F("Airspeed: "));
+    Serial.println(String(zeroVoltage));
+
+    EEPROM.get(30, ZERO_CURRENT_VOLTAGE);
+    Serial.print(F("Current: "));
+    Serial.println(String(ZERO_CURRENT_VOLTAGE));
+
+    EEPROM.get(40, ZERO_VOLTAGE);
+    Serial.print(F("Current: "));
+    Serial.println(String(ZERO_VOLTAGE));
+
+    Serial.println(F("Done retrieving calibration factors"));
+    use_prev_calibration = false;
+  }
+
+  if(zero_torque){
+    ready = false;
+    KNOWN_TORQUE = signal.toInt();
+    Serial.println(F("Calibrating torque sensor"));
+    calibrate_hx711(TorqueSensor, KNOWN_TORQUE, 0);
+    Serial.println(F("Done calibrating torque sensor"));
+    zero_torque = false;
+    ready = true;
+  }
+
+  if(zero_thrust){ //zero the thrust and the other analog sensors
+    ready = false;
+    KNOWN_THRUST = signal.toInt();
+
+    Serial.println(F("Calibrating thrust sensor"));
+    calibrate_hx711(ThrustSensor, KNOWN_THRUST, 10);
+    Serial.println(F("Done Calibrating thrust sensor"));
+
+    zero_thrust = false;
+    ready = true;
+  }
+
+  if(zero_analog_sensors){
+    ready = false;
+    Serial.println(F("Zeroing the airspeed sensor"));
+    zeroVoltage = zero_analog([]() {return analogRead(AIRSPEED_PIN) * (Vcc / 1023);}, 20);
+    Serial.println(F("Done zeroing airspeed sensor"));
+
+    Serial.println(F("Zeroing the current sensor"));
+    ZERO_CURRENT_VOLTAGE = zero_analog([]() {return analogRead(CURRENT_PIN) * (Vcc / 1023);}, 30);
+    Serial.println(F("Done zeroing current sensor"));
+
+    Serial.println(F("Zeroing the voltage sensor"));
+    ZERO_VOLTAGE = zero_analog([]() {return analogRead(VOLTAGE_PIN) * (Vcc / 1023);}, 40);
+    Serial.println(F("Done zeroing voltage sensor"));
+    
+    zero_analog_sensors = false;
+    ready = true;
+  }
+
+  if(new_file_created){ //create a new file
+    String file_name = "TEST_" + signal + ".csv";
+    data_file = SD.open(file_name, FILE_WRITE); //create the file
+    data_file.println("Current (A), Voltage (V), Torque (N.mm), Thrust (N), RPM, Airspeed (m/s)"); //set up csv headers
+    new_file_created = false;
+  }
+
+  if(marker_sent){
+    MARKERS = signal.toInt();
+    Serial.println(String(MARKERS));
+    marker_sent = false;
+  }
+
+  if(data_file){
+    if(reading_on){ //if a file exists and data logging/testing is turned on
+      //RPM SENSOR READING AND CALCULATION; Can increase precision by adding more markers
+      if(millis() >= prev_second + 250){
+        RPM = (objects / (MARKERS * 2)) * 240.0;
+        objects = 0;
+        prev_second = millis();
+      }
+      
+      if(TorqueSensor.update() && ThrustSensor.update()){
+        //CURRENT/VOLTAGE SENSOR READING
+        int current_value_in = analogRead(CURRENT_PIN);
+        int voltage_value_in = analogRead(VOLTAGE_PIN);          
+
+        float voltage = VOLTAGE_CALIBRATION * ((voltage_value_in * (Vcc / 1023.0)) - ZERO_VOLTAGE);
+
+        float current_voltage = current_value_in * (Vcc / 1023.0);
+        float current = (current_voltage - ZERO_CURRENT_VOLTAGE) / CURRENT_SENSITIVITY;
+        
+        float prev = measurements[0];
+        measurements[0] = current;
+        for(int i = 1; i < 5; i++){
+          float old = measurements[i];
+          measurements[i] = prev;
+          prev = old;
+        }
+
+        float sum = 0;
+        for(int i = 0; i < 5; i++){
+          sum += measurements[i];
+        }
+        float average_current = sum / 5.0;
+
+        //AIRSPEED SENSOR READING
+        int raw = analogRead(AIRSPEED_PIN);
+        float airspeed_voltage = raw * (Vcc / 1023.0);
+
+        float pressure_kPa = (airspeed_voltage - zeroVoltage) / sensitivity; // Convert voltage to differential pressure in kPa
+        float pressure_Pa = pressure_kPa * 1000.0; // Convert kPa to Pascals
+
+        float airspeed = 0.0;          
+        if (pressure_Pa > 0) {
+          airspeed = sqrt((2.0 * pressure_Pa) / airDensity); // Compute airspeed using Bernoulli equation
+        }
+
+        //THRUST AND TORQUE SENSOR READINGS
+        float torque_data = TorqueSensor.getData();  
+        float thrust_data = ThrustSensor.getData();  
+
+        //RATE LIMIT THE WRITING TO AVOID OVERLOADING AND KEEP CONSISTENT DATAPOINTS
+        if(!paused && millis() > last_serial_timestamp + SERIAL_PRINT_INTERVAL){     
+          last_serial_timestamp = millis();
+
+          Serial.print(F("Current: ")); Serial.print(average_current);
+          Serial.print(F(" | Voltage: ")); Serial.print(voltage);
+          Serial.print(F(" | Torque: ")); Serial.print(torque_data);
+          Serial.print(F("| Thrust: ")); Serial.print(thrust_data);
+          Serial.print(F(" | RPM: ")); Serial.print(RPM);
+          Serial.print(F(" | AIRSPEED: ")); Serial.println(airspeed);
+          Serial.print(F(" | MEMORY: ")); Serial.println(free_memory());
+
+          data_file.print(average_current); data_file.print(", "); 
+          data_file.print(voltage); data_file.print(", ");
+          data_file.print(torque_data); data_file.print(", ");
+          data_file.print(thrust_data); data_file.print(", ");
+          data_file.print(RPM); data_file.print(", ");
+          data_file.println(airspeed);
+          data_file.flush();
+        }
+      }
+
+      //increment a rotation counter when the tachometer sees a marker pass by)
+      increment();
+    }
+    else if(stop){ //If the signal to stop testing is recieved from master, close the file
+      data_file.close();
+      setup();
+    }
+  }
+}
